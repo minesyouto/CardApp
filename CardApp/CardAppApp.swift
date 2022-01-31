03599bdd51c957cecd2da2618ef7a394010214b3
minesyouto <minesyouto8833@gmail.com>
2022-01-31 14:29:09 +0900

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by みねた on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
